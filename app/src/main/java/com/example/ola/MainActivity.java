e07291d3413217fe53aa965dd12482f8384b590e
PanaSebs <g.sebastian5425@gmail.com>
2023-03-20 18:52:43 -0500

    cambio de color

--- app/src/main/java/com/example/ola/MainActivity.java
+++ app/src/main/java/com/example/ola/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.ola;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View mainLayout = findViewById(R.id.main_layout);
+        View colorButton = findViewById(R.id.color_button);
+
+        colorButton.setOnClickListener(v -> {
+            int newColor = generateRandomColor();
+            mainLayout.setBackgroundColor(newColor);
+        });
+        }
+
+        private int generateRandomColor() {
+            Random random = new Random();
+            int red = random.nextInt(256);
+            int green = random.nextInt(256);
+            int blue = random.nextInt(256);
+            return Color.rgb(red, green, blue);
+        }
     }
-}
